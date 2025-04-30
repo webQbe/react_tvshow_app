ade340fda0a76ac183cc1d62ca0abafdadacfefc
webQbe <webcubesolutionslk@gmail.com>
2025-05-01 01:49:53 +0530

    Displayed character image & name, person name, birthdate& alive/dead status

--- src/components/characters/CharacterItem.jsx
+++ src/components/characters/CharacterItem.jsx
@@ -2,8 +2,35 @@ import React from 'react'
 
 const CharacterItem = ({ item }) => { // Receive item as prop
 
+    console.log(item) // Log item object
+
     // Render character info
-  return <div>{ item.character.name }</div>
+  return (
+        <div className="card">
+            <div className="card-inner">
+                <div className="card-front">
+                    <img src={ item.character.image.medium } alt="" />
+                </div>
+                <div className="card-back">
+                    <h1>{ item.character.name }</h1>
+                    <ul>
+                        <li>
+                            <strong>Actor Name:</strong>{' '}
+                            { item.person.name }
+                        </li>
+                        <li>
+                            <strong>Birthday:</strong>{' '}
+                            { item.person.birthday }
+                        </li>
+                        <li>
+                            <strong>Status:</strong>{' '}
+                            { item.person.deathday === null ? "Alive" : "Dead" }
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    )
 
 }
 
