f1b3a9de32c55de173b4b23f6bc751dc80b5e2c2
webQbe <webcubesolutionslk@gmail.com>
2025-04-30 21:30:56 +0530

    Fetch cast data from TVMaze API

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,25 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 import Header from './components/ui/Header' // import Header component
 import './App.css'
 
 const App = () => {
+
+  /* Define states */
+  const [items, setItems] = useState([])
+  const [isLoading, setIsLoading] = useState(true)
+
+  useEffect(() => { // Run once upon loading
+
+    // Fetch cast data from TVMaze API
+    const fetchItems = async () => {
+      const result = await axios(`https://api.tvmaze.com/shows/169/cast`)
+
+      console.log(result.data) // Log response data
+    }
+
+    fetchItems()
+  },[])
 
   return (
     <div className="container">
